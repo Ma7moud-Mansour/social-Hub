c21fbf45786ca3df5139dd8374238830ba263df2
mahmoud <mahmoudabdelkareem.info@gmail.com>
2024-09-24 07:20:49 +0300

    Make like and unlike option

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User, auth
 from django.contrib.auth import logout as auth_logout
 from django.contrib.sessions.models import Session
 from django.contrib import messages
-from .models import Profile, Post
+from .models import Profile, Post, LikePost
 
 
 # Create your views here.
@@ -32,6 +32,26 @@ def upload(request):
 
         return redirect('/')
     else:
+        return redirect('/')
+
+def Like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
         return redirect('/')
 
 @login_required(login_url='signin')
